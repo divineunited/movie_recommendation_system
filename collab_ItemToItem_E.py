a5af126fb555af467507f2b1fa2e280d4837b5b1
DivineUnited <DanielDukeVu@outlook.com>
2019-05-09 16:31:10 +0800

    cleaned up collab_itemtoitem on master

--- collab_ItemToItem_E.py
+++ collab_ItemToItem_E.py
@@ -2,6 +2,51 @@ import pandas as pd
 import numpy as np
 from scipy.spatial.distance import hamming 
 
+##--------HELPER FUNCTION: FIND NEAREST ITEM NEIGHBOURS
+
+def nearestneighbours(movie_id, k, userItemRatingMatrix, distance_measure = hamming):
+    '''This helper function finds the K nearest neighbor item to a specific user's item using a selected distance measure.''' 
+
+    # create a movie df that contains all movies except the selected movie input
+    allMovies = pd.DataFrame(userItemRatingMatrix.index)
+    allMovies = allMovies[allMovies.movie_id != movie_id]
+    
+    # Add a column to this df which contains distance of active movie to each and every movie
+    allMovies["distance"] = allMovies["movie_id"].apply(lambda x: distance_measure(userItemRatingMatrix.loc[movie_id], userItemRatingMatrix.loc[x]))
+    
+    # sort the values of all movies based on the distance metric
+    sortedMovies = allMovies.sort_values(["distance"],ascending=True)["movie_id"][:k]
+    return list(sortedMovies)
+
+
+####------MAIN FUNCTION TO GET RANKED RECOMMENDATIONS
+
+def your_item_to_item_recommendations(movie_ids, data):
+    '''This function requires the user to input a list of 4 favourite movie ids. It makes use of the helper function nearestneighbours that finds the K nearest neighbor items of each movie. These items are added to a recommendation list and a ranked recommendation list is returned''' 
+
+    # We create userItemRatingMatrixforItem where index is movie_id 
+    userItemRatingMatrix = pd.pivot_table(data, values='rating',
+                                    index=['movie_id'], columns=['user_id'])
+    
+    # Loop through my favourites list and use movie_id as inputs into nearestneighbours function. 
+    # Create empty list of recommendation
+    # Append the function's output (sortedMovies) to the recommendations list
+
+    recommendations= []
+    for movie_id in movie_ids:
+        sortedMovies = nearestneighbours(movie_id, 10, userItemRatingMatrix, distance_measure = hamming) 
+        recommendations += sortedMovies
+    
+    # we want to sort according to the movies that appeared in all 4 recommendation list
+    recommendations=sorted(recommendations, key=recommendations.count, reverse=True)
+
+    # removing duplicates:
+    recommendations = list(dict.fromkeys(recommendations))
+
+    return recommendations
+
+
+
 #### ------------ FUNCTION TESTING:
 
 ##### Importing data that will be used to create recommendations:
@@ -23,64 +68,13 @@ UsersPerMovie = data.movie_id.value_counts()
 condition = data['movie_id'].isin(UsersPerMovie[UsersPerMovie > 10].index)
 data = data[condition]
 
-# Now, we have our user-item rating matrix. We have 943 users and 1,118 movies. We started off with 1,682 movies, but we whittled down the movie list to 1,118 by removing movies that had less than 10 reviews.##### Collaborative Filtering using ITEM TO ITEM SIMILARITY:
+# Now, we have our user-item rating matrix. We have 943 users and 1,118 movies. We started off with 1,682 movies, but we whittled down the movie list to 1,118 by removing movies that had less than 10 reviews.
 
 
-##--------HELPER FUNCTION: FIND NEAREST ITEM NEIGHBOURS
-
-def nearestneighbours(movie_id, k, data, distance_measure = hamming):
-    '''This helper function finds the K nearest neighbor item to a specific user's item using a selected distance measure.''' 
-    
-    # We create userItemRatingMatrixforItem where index is movie_id 
-    userItemRatingMatrixforItem=pd.pivot_table(data, values='rating',
-                                    index=['movie_id'], columns=['user_id'])
-
-
-    # create a movie df that contains all movies except the selected movie input
-    allMovies = pd.DataFrame(userItemRatingMatrixforItem.index)
-    allMovies = allMovies[allMovies.movie_id != movie_id]
-    
-    # Add a column to this df which contains distance of active movie to each and every movie
-    allMovies["distance"] = allMovies["movie_id"].apply(lambda x: distance_measure(userItemRatingMatrixforItem.loc[movie_id], userItemRatingMatrixforItem.loc[x]))
-    
-    
-    # sort the values of all movies based on the distance metric
-    sortedMovies = allMovies.sort_values(["distance"],ascending=True)["movie_id"][:k]
-    return list(sortedMovies)
-
-
-####------MAIN FUNCTION TO GET RANKED RECOMMENDATIONS
-
-def your_item_to_item_recommendations(my_favourites_list):
-    '''This function requires the user to input a list of favourite movie ids. It makes use of the helper function nearestneighbours that finds the K nearest neighbor items of each movie. These items are added to a recommendation list and a ranked recommendation list is returned''' 
-
-    
-    #Loop through my favourites list and use movie_id as inputs into nearestneighbours function. 
-    #Create empty list of recommendation
-    #Append the function's output (sortedMovies) to the recommendations list
-
-    for i in range(0, len(my_favourites_list)):
-        my_favourites_list[i]= int(my_favourites_list[i])
-
-    recommendations= []
-    for movie_id in my_favourites_list:
-
-        sortedMovies = nearestneighbours(movie_id, 10, data, distance_measure = hamming) 
-        
-    # #the output of this function isvsortedMovies
-        recommendations = recommendations + sortedMovies
-
-    #we want to sort according to the movies that appeared in all 4 recommendation list
-
-    recommendations=sorted(recommendations,key=recommendations.count, reverse=True)
-
-    return recommendations
-
-   
-
+##### Collaborative Filtering using ITEM TO ITEM SIMILARITY:
 
 my_list = [522, 523, 524, 525]
-print(your_item_to_item_recommendations(my_list))
+print(your_item_to_item_recommendations(my_list, data))
 
 
 
